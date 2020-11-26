e787881a379e7e88a6eeb36c790939ef6b9f7fc9
3lCR3P3R <3lCR3P3R@users.noreply.github.com>
2020-11-26 05:21:15 -0500

    Se finalizo el minijuego Chart Course
    Minijuego del area de Navegacion, donde mueves una nave

--- src/components/minigames/ChartCourse.js
+++ src/components/minigames/ChartCourse.js
@@ -9,6 +9,12 @@ class ChartCourse extends React.Component {
     this.ctx = ""
     this.imagenFondo = "";
     this.imagenNave = "";
+    this.nivel = 0;
+    this.grados = -45;
+    this.limiteIzq = -280;
+    this.limiteIzqDos = -240;
+    this.limiteDer = 30;
+    this.limiteDerDos = 200;
     this.srcImagenNave = "https://i.imgur.com/V62kLgI.png";
     this.srcImagenFondo = "https://i.imgur.com/AqBMGZm.png";
     this.arrastrar = false;
@@ -17,8 +23,8 @@ class ChartCourse extends React.Component {
         y: 0
     };
     this.nave = {
-        posX: -210,
-        posY: 400,
+        posX: -239,
+        posY: 385,
         ancho: 103,
         alto: 77
     } 
@@ -50,66 +56,123 @@ class ChartCourse extends React.Component {
 
     dibujarNave(img, { posX, posY, ancho, alto}){
         this.ctx.save();
-        this.ctx.rotate(-40 * Math.PI / 180);
+        this.ctx.rotate(this.grados * Math.PI / 180);
         this.ctx.drawImage(img, posX, posY, ancho, alto);
         this.ctx.restore();
-
     }
 
     oMousePos(evt) {
         var rect = this.canvas.current.getBoundingClientRect();
-        console.log(rect)
-        debugger
+        // console.log(rect)
         return { // devuelve un objeto
             x: Math.round(evt.clientX - rect.left),
             y: Math.round(evt.clientY - rect.top)
         };
+    }
+
+    cambiarValores(){
+        switch (this.nivel) {
+            case 0:
+                this.nivel = 0;
+                this.grados = -45;
+                this.limiteIzq = -280;
+                this.limiteIzqDos = -240;
+                this.limiteDer = 30;
+                this.limiteDerDos = 200;
+                this.nave.posX = -239
+                this.nave.posY = 385
+                this.clear();
+                this.dibujarNave(this.imagenNave, this.nave);
+                break;
+            case 1:
+                this.grados = 38;
+                this.limiteIzq = 340;
+                this.limiteIzqDos = 380;
+                this.limiteDer = 628;
+                this.limiteDerDos = 900;
+                this.nave.posX = 381
+                this.nave.posY = -70
+                this.clear();
+                this.dibujarNave(this.imagenNave, this.nave);
+                break;
+            case 2:
+                this.grados = -45;
+                this.limiteIzq = 20;
+                this.limiteIzqDos = 60;
+                this.limiteDer = 342;
+                this.limiteDerDos = 525;
+                this.nave.posX = 61
+                this.nave.posY = 635
+                this.clear();
+                this.dibujarNave(this.imagenNave, this.nave);
+                break;
+            case 3:
+                this.grados = 39;
+                this.limiteIzq = 500;
+                this.limiteIzqDos = 660;
+                this.limiteDer = 915;
+                this.limiteDerDos = 1100;
+                this.nave.posX = 661;
+                this.nave.posY = -355;
+                this.clear();
+                this.dibujarNave(this.imagenNave, this.nave);
+                break;
+        
+            default:
+                break;
+        }
     }
     
     handleClickDown = (e) => {
         // this.startTime = new Date();
         // this.moverRectangulo(this.refuelStation);
         var mousePos = this.oMousePos(e);
-        if (this.ctx.isPointInPath(mousePos.x, mousePos.y)) {
             this.arrastrar = true;
             console.log("abajo");
             this.delta.x = this.nave.posX - mousePos.x;
             this.delta.y = this.nave.posY - mousePos.y;
-        }
     }
 
     handleClickMove = (e) => {
         // this.startTime = new Date();
-        // this.moverRectangulo(this.refuelStation);
-        var mousePos = this.oMousePos(e);
-        console.log("moviendo");
-        
         if (this.arrastrar) {
-            // if (this.nave.posX >= -210 && this.nave.posX <= 100) {
-            //     alert("K pro 😎👌");
-            //     this.ctx.clearRect(0, 0, 1116, 666);
-            //      this.pintarImagenDeFondo(this.imagenFondo);
-            //     this.nave.posX = -210;
-            // this.dibujarNave(this.imagenNave, this.nave);
-            // }
-            this.ctx.clearRect(0, 0, 1116, 666);
-            this.pintarImagenDeFondo(this.imagenFondo);
-            this.nave.posX = mousePos.x + this.delta.x
-            this.dibujarNave(this.imagenNave, this.nave);
+            var mousePos = this.oMousePos(e);
+
+            if (this.nave.posX >= this.limiteIzq && this.nave.posX <= this.limiteIzqDos) {
+                this.arrastrar = false
+                this.nave.posX = this.limiteIzqDos + 2
+                console.log("limite")
+            } else {
+                this.arrastrar = true
+                this.clear();
+                this.nave.posX = mousePos.x + this.delta.x
+                this.dibujarNave(this.imagenNave, this.nave);
+                if (this.nave.posX >= this.limiteDer && this.nave.posX <= this.limiteDerDos) {
+                    this.arrastrar = false
+                    this.nave.posX = this.limiteDer + 1
+                    if (this.nivel === 3) {
+                        alert("Tarea Completada 😎👌")
+                    }
+                    this.nivel++;
+                    console.log("limite xdxd")
+                }
+            }
         }
-    }
+    }   
 
     handleClickUp = () => {
         // this.endTime = new Date();
-        // clearInterval(this.interval);
-        // this.interval = null;
         this.arrastrar = false;
+        this.cambiarValores()
         console.log("arriba");
+    }
+
+    // this.ctx.drawImage(this.imagenNave, e.clientX - this.nave.posX, this.nave.posY, this.nave.ancho, this.nave.alto);
+    clear(){
         this.ctx.clearRect(0, 0, 1116, 666);
         this.pintarImagenDeFondo(this.imagenFondo);
-        this.dibujarNave(this.imagenNave, this.nave);
+        // this.dibujarNave(this.imagenNave, this.nave);
     }
-    
 
   render() {
       return (
